887ceb7df28e0a94e3b6f2da16a92c0e6bac53f1
safizn <safizn.us@gmail.com>
2024-12-06 08:53:06 +0200

    record versions

--- script/dev.sh
+++ script/dev.sh
@@ -27,3 +27,15 @@ build() {
     NO_STRIP=true cargo tauri build 
     WEBKIT_DISABLE_COMPOSITING_MODE=1 ./src-tauri/target/release/bundle/appimage/*.AppImage
 }
+
+record_version() { 
+    NODE_VERSION=$(node -v | cut -d 'v' -f2)
+    PNPM_VERSION=$(pnpm --version | cut -d ' ' -f2)
+    RUST_VERSION=$(rustc --version | awk '{print $2}') 
+    CARGO_VERSION=$(cargo --version | awk '{print $2}')
+
+    echo "Node.js version: ${NODE_VERSION}" > version.txt
+    echo "pnpm version: ${PNPM_VERSION}" >> version.txt
+    echo "Rust version: ${RUST_VERSION}" >> version.txt
+    echo "Cargo version: ${CARGO_VERSION}" >> version.txt
+}
