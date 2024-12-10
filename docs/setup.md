dc105efaff4ee311ea223955422acfcf0e58c711
safizn <nassarsafi@gmail.com>
2024-12-10 17:20:37 +0200

    ci: release-please workflow

--- docs/setup.md
+++ docs/setup.md
@@ -1,3 +1,6 @@
 ## setup repo guide for local development: 
 - `./script.sh provision_tauri
 - install dependencies for Tauri mobile targets: https://v2.tauri.app/start/prerequisites/#configure-for-mobile-targets
+
+- update squash message settings for pull request on github to "pull request title & commit": https://github.blog/changelog/2022-05-11-default-to-pr-titles-for-squash-merge-commit-messages/
+
--- script/deploy.sh
+++ script/deploy.sh
@@ -53,6 +53,56 @@ manual_release_package() {
     git stash pop > /dev/null 2>&1
 }
 
+example_workflow_with_release_please_manually_triggered() { 
+    create_feature_pr() {
+        feature_branch=$1
+        git checkout -b $feature_branch
+        git commit --allow-empty -m "commit 1" 
+        git commit --allow-empty -m "commit 2" 
+        git commit --allow-empty -m "commit 3"
+        git push --set-upstream origin $feature_branch
+        gh pr create --base main --head $feature_branch --title "feat: adding feacture x to component A" 
+    }
+
+    merge_last_pr() { 
+        local feature_branch=$1
+        git fetch origin 
+        git checkout main 
+        last_pr_number=$(gh pr list --state open --json number | jq -r '.[0].number') 
+        default_branch=$(git remote show origin | grep "HEAD branch:" | awk '{print $3}')
+        pr_title=$(gh pr view "$last_pr_number" --json title | jq -r '.title')
+        gh pr merge $last_pr_number --squash -t "$pr_title"
+        git pull && git push origin main 
+    }
+
+    local release_please_workflow=monorepo-release.yml
+
+    local feature_branch=branch_feature_x
+    create_feature_pr $feature_branch
+
+    # create release pr
+    merge_last_pr $feature_branch # merge feature pr
+    gh workflow run $release_please_workflow # -> release pr is created. 
+    gh run list --workflow=$release_please_workflow 
+
+    {
+        # new features can be added and will be appended to the existing release PR managed by `release-please``.
+        local feature_branch=branch_feature_y
+        create_feature_pr $feature_branch
+        
+        # create release pr
+        merge_last_pr $feature_branch # merge feature pr
+        gh workflow run $release_please_workflow # -> release pr is created. 
+        gh run list --workflow=$release_please_workflow 
+
+        merge_last_pr $feature_branch # merge release pr
+    }
+
+    # create a package release on Github
+    merge_last_pr $feature_branch # merge release pr
+    gh workflow run $release_please_workflow # -> package a github release
+}
+
 delete_tag() { 
     tag=${1:-web-server-v0.1.1}
     git push origin :$tag
