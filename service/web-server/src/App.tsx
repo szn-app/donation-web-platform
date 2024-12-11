da7d4afd5fde2eea30959088fbffe2d5d8c0d37e
safizn <nassarsafi@gmail.com>
2024-12-11 23:38:22 +0200

    feat: form for rust execution (dummy commit)

--- service/web-server/src/App.tsx
+++ service/web-server/src/App.tsx
@@ -14,20 +14,6 @@ function App() {
 
   return (
     <main className="container">
-      <h1>In-kind Donation Platform</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
       <p>Try Rust code execution:</p> 
       <form
         className="row"
@@ -44,6 +30,22 @@ function App() {
         <button type="submit">Greet</button>
       </form>
       <p>{greetMsg}</p>
+
+
+      <h1>In-kind Donation Platform</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
     </main>
   );
 }
