c6f1fe52960fb101ad56790b325022d040d47668
safizn <nassarsafi@gmail.com>
2024-12-11 23:35:17 +0200

    ci: fix shell script loading

--- script.sh
+++ script.sh
@@ -4,8 +4,9 @@
 # `./script.sh <function-name> [args...]`
 
 # load functions
-directory=./script
-for file in "$directory/*.sh"; do
+directory="./script"
+for file in "$directory"/*.sh; do
+  echo "source $file"
   source $file
 done
 
@@ -15,7 +16,6 @@ done
     if [[ $# -lt 1 ]]; then
         # This case can be used for executing $`source ./script.sh` to load functions to current shell session.
         echo "source ./script.sh to load functions to current shell session"
-        exit 0
     elif ! declare -f "$fn_name" || ! [[ $(type -t "$fn_name") ]]; then # check if defined in current file or sourced declaration 
         echo "Error: function '$fn_name' not declared. "
         exit 1
