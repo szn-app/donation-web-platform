bf337bb7cfbf6a32f9359a8edfb71ab46909f97c
safizn <nassarsafi@gmail.com>
2024-12-11 18:58:02 +0200

    feat: title app

--- service/web-server/src/App.tsx
+++ service/web-server/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>In-kind Donation Platform</h1>
 
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
@@ -27,7 +27,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
         className="row"
