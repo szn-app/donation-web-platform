e70c31018e23644dcfd71e055cb6c7016c7d96e8
safizn <nassarsafi@gmail.com>
2024-12-13 23:41:47 +0200

    changed message

--- service/web-server/src-tauri/src/lib.rs
+++ service/web-server/src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Rust code executed with {} parameter (using Tauri frontend execution model)", name)
+    format!("Rust code executed with {} parameter (using Tauri frontend execution model.)", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
