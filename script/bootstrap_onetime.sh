f58754afc03b04ee8f5ec489d86ddb6c82e24b36
safizn <nassarsafi@gmail.com>
2025-01-09 16:46:04 +0200

    chore: install Shadcn component kit

--- script/bootstrap_onetime.sh
+++ script/bootstrap_onetime.sh
@@ -1,10 +1,19 @@
 install_tailwindcss() { 
     pnpm install -D tailwindcss postcss autoprefixer
-    pnpm tailwind init -p 
-
+    pnpm dlx tailwindcss init -p
+    
     pnpm install -D prettier prettier-plugin-tailwindcss
 
     example() { 
         pnpm tailwindcss -i ./src/input.css -o ./src/output.css --watch
     }
 }
+
+install_shadcn_for_vite() { 
+    install_tailwindcss
+
+    # follow https://ui.shadcn.com/docs/installation/vite
+    # [manual] setup configs and paths resolution
+
+    pnpm dlx shadcn@latest init
+}   
