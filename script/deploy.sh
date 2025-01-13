4a3b93ee508d391059fdecbc6496314547c8e06e
safizn <nassarsafi@gmail.com>
2025-01-14 00:14:04 +0200

    fix: secret parsing using secretGenerator kustomize property

--- script/deploy.sh
+++ script/deploy.sh
@@ -147,9 +147,9 @@ env_files() {
         auth_ui_secret_file="./manifest/auth_ui/production/secret.env"
         if [ ! -f "$auth_ui_secret_file" ]; then
             t=$(mktemp) && cat <<EOF > "$t"
-COOKIE_SECRET=$(openssl rand -base64 32)
-CSRF_COOKIE_NAME=$(shuf -n 1 /usr/share/dict/words | tr -d '\n')_csrf 
-CSRF_COOKIE_SECRET=$(openssl rand -base64 32)       
+COOKIE_SECRET="$(openssl rand -base64 32)"
+CSRF_COOKIE_NAME="$(shuf -n 1 /usr/share/dict/words | tr -d '\n')_csrf"
+CSRF_COOKIE_SECRET="$(openssl rand -base64 32)"
 EOF
 
             mv $t $auth_ui_secret_file
