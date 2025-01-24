6b006c72ca06569f73c5338d293b8178473be03e
safizn <nassarsafi@gmail.com>
2025-01-24 21:39:14 +0200

    ci(infrastructure): Prometheus + Grafana installation

--- script/provision_cloud.sh
+++ script/provision_cloud.sh
@@ -423,6 +423,110 @@ EOF
   kubectl rollout restart deploy/coredns -n kube-system
 }
 
+# TODO: solve issue of high resource utilization by Prometheus
+install_monitoring() {
+  # https://artifacthub.io/packages/helm/prometheus-community/prometheus
+  install_prometheus() { 
+    helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+    helm repo update
+
+    t="$(mktemp)-prometheus-values.yml" && cat <<EOF > "$t"
+server:
+  persistentVolume:
+    storageClass: local-path
+    size: 3Gi
+alertmanager:
+  persistentVolume:
+    size: 512Mi
+resources:
+    limits:
+      cpu: 150m
+      memory: 512Mi
+    requests:
+      cpu: 80m
+      memory: 256Mi
+EOF
+
+    helm upgrade prometheus --install prometheus-community/prometheus --namespace monitoring --create-namespace -f $t
+
+    # https://github.com/prometheus-community/helm-charts/blob/main/charts/prometheus/values.yaml
+    t="$(mktemp)-monitoring-configuration.yml" && cat <<EOF > "$t"
+apiVersion: monitoring.coreos.com/v1
+kind: ServiceMonitor
+metadata:
+  name: kube-state-metrics
+  labels:
+    release: prometheus
+spec:
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: kube-state-metrics
+  endpoints:
+  - port: http-metrics
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: prometheus
+rules:
+- apiGroups: [""]
+  resources:
+  - nodes
+  - services
+  - endpoints
+  - pods
+  verbs: ["get", "list", "watch"]
+EOF
+
+  }
+
+  # https://artifacthub.io/packages/helm/grafana/grafana
+  install_grafana() { 
+    helm repo add grafana https://grafana.github.io/helm-charts
+    helm repo update
+
+    # https://github.com/grafana/helm-charts/blob/main/charts/grafana/values.yaml
+    t="$(mktemp)-grafana-values.yml" && cat <<EOF > "$t"
+persistence:
+  enabled: true
+  storageClassName: local-path
+  size: 1Gi
+service:
+  type: ClusterIP
+resources:
+  limits:
+    cpu: 80m
+    memory: 200Mi
+  requests:
+    cpu: 40m
+    memory: 128Mi
+
+EOF
+
+    helm upgrade grafana --install grafana/grafana --namespace monitoring --create-namespace -f $t
+
+  }
+
+  manual_steps() { 
+    # TODO: install Grafana extension for Prometheus from the graphical interface and implement recommendations from docs
+  }
+
+  install_prometheus
+  install_grafana
+
+  verify() {
+    kubectl get pods -l app=prometheus
+
+    # get grafana password 
+    kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+  }
+
+}
+
+install_pod_vertical_autoscaller() { 
+
+}
+
 # https://github.com/kube-hetzner/terraform-hcloud-kube-hetzner
 hetzner_cloud_provision() {
     action=${1:-"install"}
@@ -581,6 +685,8 @@ hetzner_cloud_provision() {
       restart_cert_manager  # must be restarted after installation of Gateway Api
       sleep 20
       install_storage_class 
+      # TODO: check resource limits and prevent contention when using monitoring tools
+      # install_monitoring
 
       verify_installation() {
         k9s # https://k9scli.io/topics/commands/
@@ -601,6 +707,13 @@ hetzner_cloud_provision() {
         helm get manifest nginx -n nginx 
         
         journalctl -r -n 200
+
+        # check cpu/mem utilization
+        kubectl get node && kubectl top nodes && kubectl describe node
+        kubectl top pods
+        kubectl top pods --containers=true -A --sort-by memory
+        HIGHEST_CPU_NODE=$(kubectl top nodes | awk 'NR>1 {print $1, $2+0}' | sort -k2 -nr | head -n 1 | awk '{print $1}')
+        kubectl top pods -A -n $HIGHEST_CPU_NODE --sort-by cpu
 
         ### ssh into remove machines
         # echo "" > ~/.ssh/known_hosts # clear known hosts to permit connection for same assigned IP to different server
