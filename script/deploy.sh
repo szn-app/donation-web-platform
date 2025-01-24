5f5b71a96cde62437b05a03f6a9155201c6f776c
safizn <nassarsafi@gmail.com>
2025-01-24 03:03:21 +0200

    ci(auth): fix Oauth2 and OIDC flows with example scripts

--- script/deploy.sh
+++ script/deploy.sh
@@ -203,6 +203,10 @@ install_ory_stack() {
             helm uninstall hydra -n auth
             helm uninstall postgres-hydra -n auth
             helm uninstall oathkeeper -n auth
+
+            kubectl delete secret ory-hydra-client--frontend-client-oauth -n auth
+            kubectl delete secret ory-hydra-client--frontend-client -n auth
+            kubectl delete secret ory-hydra-client--internal-communication -n auth
             return 
         fi
     }
@@ -400,13 +404,69 @@ install_ory_stack() {
             curl -i https://api.wosoom.com/p/ 
             curl -H "Accept: text/html" -X GET https://api.wosoom.com/p/ 
 
+            # NOTE: gaining authorization code process requires a browser or tool that handles consent
             # NOTE: SDK libraries for Oauth and OIDC compose requests better
             # initiate login flow and redirect to login page
-            curl -i "https://auth.wosoom.com/authorize/oauth2/auth?client_id=frontend-client&response_type=code%20id_token&scope=offline_access%20openid&redirect_url=https://wosoom.com/&state=some_random_string&nonce=some_other_random_string"
+            printf "visit in browser %s" "https://auth.wosoom.com/authorize/oauth2/auth?client_id=frontend-client&response_type=code%20id_token&scope=offline_access%20openid&redirect_url=https://wosoom.com/&state=some_random_string&nonce=some_other_random_string"
+            # using the other client
+            printf "visit in browser %s" "https://auth.wosoom.com/authorize/oauth2/auth?client_id=frontend-client-oauth&response_type=code&scope=offline_access%20openid&redirect_url=https://wosoom.com&state=some_random_string&nonce=some_random_str"
             # [manual] following the process should redirect after login with the authorization code provided in the URL
             {
-                # typically would run from within the cluster using the backend of the frontend ui application
-                curl -i -X POST "" -d "&&&&" -H "Content-Type: application/x-www-form-urlencoded"
+                # typically would run from within the cluster using the backend server of the frontend ui application (must be secure as it contains client secret)
+                # EXAMPLE for usage with client_secret_post
+
+                CLIENT_ID="frontend-client-oauth"
+                CLIENT_SECRET="WHdvNml2UkpBZmk3Q1pyQ3k5VHJKS1BNTFVMd3BoNkk="
+                AUTHORIZATION_CODE="ory_ac_OKO7yH2ONi0aA1iAU2mFBatT6QAGF2wrkKEXUlmT71w.exdzV6GyFy57zHy2ox2HD_HRBalGOL6KCVWsBS55_0Y"  # [manually eplace this] update the code from the result redirect url parameter
+                REDIRECT_URI="https://wosoom.com"
+
+                # Execute the curl request
+                tokens_payload=$(curl -k -s --request POST \
+                    --url https://auth.wosoom.com/authorize/oauth2/token \
+                    --header "accept: application/x-www-form-urlencoded" \
+                    --form "grant_type=authorization_code" \
+                    --form "code=${AUTHORIZATION_CODE}" \
+                    --form "redirect_uri=${REDIRECT_URI}" \
+                    --form "client_id=${CLIENT_ID}" \
+                    --form "client_secret=${CLIENT_SECRET}" \
+                    --form "scope=offline_access openid" | jq)
+                
+                ACCESS_TOKEN=$(echo $tokens_payload | jq -r '.access_token')
+                REFRESH_TOKEN=$(echo $tokens_payload | jq -r '.refresh_token')
+                ID_TOKEN=$(echo $tokens_payload | jq -r '.id_token')
+
+
+                example_for_client_secret_basic_method() { 
+                    CLIENT_ID="frontend-client-oauth"
+                    CLIENT_SECRET="MEhmcEoyVXc5Y3RDcXlQZndkZ1A2WXZsazdVNHZ2OHM="
+                    AUTHORIZATION_CODE="ory_ac_aMKwa8sp41LARkYAUn9XdV6ll_XSWz5TE6TGR9ge8jw.YkU6W-aX8Zs5KkG0RS9zHSHAbYhcNbWNXWU-fhmOxUc" 
+                    REDIRECT_URI="https://wosoom.com"
+                    # Base64 encode the client ID and secret
+                    BASE64_CREDENTIALS=$(echo -n "${CLIENT_ID}:${CLIENT_SECRET}" | base64 -w 0)
+                    echo $BASE64_CREDENTIALS
+                    # Construct the curl command
+                    curl -i -k -X POST \
+                        -H "Content-Type: application/x-www-form-urlencoded" \
+                        -H "Authorization: Basic ${BASE64_CREDENTIALS}" \
+                        -d "grant_type=authorization_code" \
+                        -d "code=${AUTHORIZATION_CODE}" \
+                        -d "redirect_uri=${REDIRECT_URI}" \
+                        https://auth.wosoom.com/authorize/oauth2/token
+                }
+
+                # use token to access restricted endpoint 
+                curl -i -k -H "Authorization: Bearer $ACCESS_TOKEN" https://api.wosoom.com
+
+                # request refresh token 
+                curl -k -i --request POST \
+                    --url https://auth.wosoom.com/authorize/oauth2/token \
+                    --header "accept: application/x-www-form-urlencoded" \
+                    --form "grant_type=refresh_token" \
+                    --form "refresh_token=${REFRESH_TOKEN}" \
+                    --form "redirect_uri=${REDIRECT_URI}" \
+                    --form "client_id=${CLIENT_ID}"  \
+                    --form "client_secret=${CLIENT_SECRET}" \
+                    --form "scope=offline_access openid"
             }
         }
 
@@ -419,11 +479,11 @@ create_oauth2_client_for_trusted_app() {
         example_hydra_admin() { 
             kubectl run -it --rm --image=debian:latest debug-pod --namespace auth -- /bin/bash
             {
-                apt update && apt install curl -y
                 # install hydra
+                apt update && apt install curl jq -y
                 bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -d -b . hydra v2.2.0 && mv hydra /usr/bin/
 
-                curl http://hydra-admin/admin/clients
+                curl http://hydra-admin/admin/clients | jq
 
                 delete_all_clients() { 
                     client_list=$(curl -X GET 'http://hydra-admin/admin/clients' | jq -r '.[].client_id')
@@ -453,19 +513,23 @@ EOF
             kubectl cp $t setup-pod:$t --namespace auth
             kubectl exec -it setup-pod --namespace auth -- /bin/sh -c "chmod +x $t && $t"
         }
+
+        kubectl wait --for=condition=ready pod/setup-pod --namespace=auth --timeout=300s
+        
         {
-            kubectl wait --for=condition=ready pod/setup-pod --namespace=auth --timeout=300s
 
             # app client users for trusted app
-            t="$(mktemp).sh" && cat << 'EOF' > $t
+
+            example_using_hydra() { 
+                t="$(mktemp).sh" && cat << 'EOF' > $t
 #!/bin/bash
 hydra create oauth2-client --name frontend-client-2 --audience backend-service --endpoint http://hydra-admin --grant-type authorization_code,refresh_token --response-type code --redirect-uri https://wosoom.com --scope offline_access,openid --skip-consent --skip-logout-consent --token-endpoint-auth-method client_secret_post
 EOF
-            kubectl cp $t setup-pod:$t --namespace auth
-            kubectl exec -it setup-pod --namespace auth -- /bin/bash -c "chmod +x $t && $t"
+                kubectl cp $t setup-pod:$t --namespace auth
+                kubectl exec -it setup-pod --namespace auth -- /bin/bash -c "chmod +x $t && $t"
+            }
 
 
-            # alternative approach using curl
             # redirect uri is where the resource owner (user) will be redirected to once the authorization server grants permission to the client
             # NOTE: using the `authorization code` the client gets both `accesst token` and `id token` when `scope` includes `openid`.
             client_secret="$(LC_ALL=C tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32 | base64)" 
@@ -487,8 +551,8 @@ EOF
             cat << EOF >> $t
         "grant_types": ["authorization_code", "refresh_token"],
         "response_types": ["code id_token"],
-        "redirect_uris": ["https://wosoom.com/"], 
-        "audience": ["exposed-api"],    
+        "redirect_uris": ["https://wosoom.com"], 
+        "audience": ["https://wosoom.com"],    
         "scope": "offline_access openid",
         "token_endpoint_auth_method": "client_secret_post",
         "skip_consent": true,
@@ -502,6 +566,60 @@ EOF
             # create/update secret 
             if [[ -z "$client_exist" ]]; then
                 kubectl create secret generic ory-hydra-client--frontend-client -n auth --from-literal=client_secret="$client_secret" --dry-run=client -o yaml | kubectl apply -f - 2>/dev/null
+            fi
+
+            example_alternative_option() {
+                # TODO: incomplete example
+                # https://www.ory.sh/docs/hydra/self-hosted/quickstart
+                hydra create client \
+                    --endpoint http://hydra-admin \
+                    --grant-type authorization_code,refresh_token \
+                    --response-type code,id_token \
+                    --format json \
+                    --scope openid --scope offline \
+                    --redirect-uri https://wosoom.com/ --token-endpoint-auth-method none
+
+                code_client_id=$(echo $code_client | jq -r '.client_id')
+                code_client_secret=$(echo $code_client | jq -r '.client_secret')
+            }
+
+        }
+        {
+
+            client_secret="$(LC_ALL=C tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32 | base64)" 
+            client_exist=$(curl -s 'http://hydra-admin/admin/clients' | jq -r '.[] | select(.client_id=="frontend-client-oauth") | .client_id')
+            t="$(mktemp).sh" && cat << 'EOF' > $t
+#!/bin/bash
+
+client_exist=$(curl -s 'http://hydra-admin/admin/clients' | jq -r '.[] | select(.client_id=="frontend-client-oauth") | .client_id')
+
+if [[ -z "$client_exist" ]]; then
+echo 'Adding oauth2 client'
+
+curl -X POST 'http://hydra-admin/admin/clients' -H 'Content-Type: application/json' \
+    --data '{
+        "client_id": "frontend-client-oauth",
+        "client_name": "frontend-client-oauth",
+EOF
+            echo "\"client_secret\": \"$client_secret\"," >> $t
+            cat << EOF >> $t
+        "grant_types": ["authorization_code", "refresh_token"],
+        "response_types": ["code"],
+        "redirect_uris": ["https://wosoom.com"], 
+        "audience": ["https://wosoom.com"],    
+        "scope": "offline_access openid",
+        "token_endpoint_auth_method": "client_secret_post",
+        "skip_consent": true,
+        "skip_logout_prompt": true,
+        "post_logout_redirect_uris": []
+    }'
+fi
+EOF
+            kubectl cp $t setup-pod:$t --namespace auth
+            kubectl exec -it setup-pod --namespace auth -- /bin/sh -c "chmod +x $t && $t"
+            # create/update secret 
+            if [[ -z "$client_exist" ]]; then
+                kubectl create secret generic ory-hydra-client--frontend-client-oauth -n auth --from-literal=client_secret="$client_secret" --dry-run=client -o yaml | kubectl apply -f - 2>/dev/null
             fi
 
             example_alternative_option() {
@@ -542,7 +660,7 @@ EOF
             cat << EOF >> $t
         "grant_types": ["client_credentials"],
         "response_types": [],
-        "redirect_uris": ["https://wosoom.com/"], 
+        "redirect_uris": ["https://wosoom.com"], 
         "audience": ["internal-api", "exposed-api"],
         "scope": "offline_access openid custom_scope:read",
         "token_endpoint_auth_method": "client_secret_basic",
