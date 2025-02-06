236eaca4fe0ace4009e49df7b005e9e94ffa4814
safizn <nassarsafi@gmail.com>
2025-02-06 13:27:54 +0200

    fix: remove example tests and files

--- service/web-server/src/__tests__/Order.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { render } from "@testing-library/react";
-import Order from "../example/components/Order";
-
-test("snapshot with nothing in order", () => {
-  const { asFragment } = render(<Order />);
-  expect(asFragment()).toMatchSnapshot();
-});
--- service/web-server/src/example/api/getPastOrders.tsx
+++
@@ -1,8 +0,0 @@
-export default async function getPastOrders(page: number) {
-  const response = await fetch(`/api/past-orders?page=${page}`);
-  const data = await response.json();
-
-  return data;
-}
-
-// usage: https://github.com/btholt/citr-v9-project/blob/main/10-query/src/routes/past.lazy.jsx#L12
