a0e909113eae5ca4c67cb5e92a709e25796492d5
safizn <nassarsafi@gmail.com>
2025-02-08 13:56:32 +0200

    fix(docker): rename image

--- service/auth-token-exchange/script/docker.sh
+++ service/auth-token-exchange/script/docker.sh
@@ -7,9 +7,9 @@ misc_auth_token_exchange() {
 # IMPORTANT! used by release.yml workflow
 build_container_auth_token_exchange() { 
     # NOTE: uses buildx (instead of the legacy build)
-    docker build . -t auth_token_exchange:latest
+    docker build . -t auth-token-exchange:latest
 }
 
 run_docker_auth_token_exchange() {
-    docker run -d -p 80:3000 auth_token_exchange
+    docker run -d -p 80:3000 auth-token-exchange
 }
