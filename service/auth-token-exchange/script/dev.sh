674b4ffa4e81aa697f3ec343eb83ab3f20e966b3
safizn <nassarsafi@gmail.com>
2025-02-11 12:30:06 +0200

    feat: auth-token-exchange fallback api implementation

--- service/auth-token-exchange/script/dev.sh
+++ service/auth-token-exchange/script/dev.sh
@@ -1,6 +1,13 @@
 test() { 
-    cargo watch -q -c -w src/ -x run 
+    cargo watch -q -c -w src/ -x run
     cargo watch -q -c -w tests/ -x "test -q test_main -- --nocapture" 
+}
+
+single_test() { 
+    cargo watch -q -c -w src/ -x run &
+    sleep 1s
+    cargo test -q test_main -- --nocapture 
+    kill $(jobs -p)
 }
 
 bootstrap() { 
--- service/auth-token-exchange/src/main.rs
+++ service/auth-token-exchange/src/main.rs
@@ -12,9 +12,7 @@ use std::net::SocketAddr;
 #[tokio::main]
 async fn main() {
     // Build our application with a single route
-    let app = Router::new()
-        .route("/health/status", get(health_status))
-        .merge(ouath2_routes());
+    let app = routes();
 
     // Run our application
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -22,8 +20,15 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-fn ouath2_routes() -> Router {
-    Router::new().route("/oauth2/token", get(oauth2_token))
+fn routes() -> Router {
+    fn ouath2_routes() -> Router {
+        Router::new().route("/oauth2/token", get(oauth2_token))
+    }
+
+    Router::new()
+        .route("/health/status", get(health_status))
+        .merge(ouath2_routes())
+        .fallback(handler_404)
 }
 
 #[derive(serde::Deserialize, Debug)]
@@ -38,11 +43,21 @@ struct params {
 async fn health_status(Query(params): Query<params>) -> impl IntoResponse {
     dbg!(&params);
     let id = params.client_id.as_deref().unwrap_or("NO ID RECEIVED");
+    println!("--> Health status endpoint called with client_id: {}", id);
 
     Html(format!("Hello, {}", id)).into_response()
-    // StatusCode::OK
 }
 
 async fn oauth2_token() -> impl IntoResponse {
+    println!("--> OAuth2 token endpoint called");
     StatusCode::OK
+}
+
+async fn handler_404() -> impl IntoResponse {
+    println!("--> fallback 404 handler called");
+
+    (
+        StatusCode::NOT_FOUND,
+        Html("<h1>404</h1><p>Nothing to see here</p>"),
+    )
 }
